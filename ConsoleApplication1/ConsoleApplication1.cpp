12fe16b21505e4566cce4c0eed15ac2907b367ef
SanyaKor <58567525+SanyaKor@users.noreply.github.com>
2020-12-23 14:12:21 +0300

    fixed minor bugs, rewritten menu

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -23,8 +23,12 @@ vector<fs::path> there_can_be_only_one(vector<string>pa, vector<fs::path>rem)
     vector<fs::path>res;
     for (int i = 0; i < rem.size(); i++)
     {
-        if (fs::exists(rem[i]))
+        if (fs::exists(rem[i])) {
+            if (fs::is_directory(rem[i])) {
+                fs::remove_all(rem[i]);
+            }
             fs::remove(rem[i]);
+        }
     }
 
     for (auto& p : fs::recursive_directory_iterator(pa[0]))
@@ -48,33 +52,38 @@ vector<fs::path> there_can_be_only_one(vector<string>pa, vector<fs::path>rem)
     return res;
 }
 
+
 vector<fs::path> we_are_conected_by_the_one_feeling(vector<string>pa, vector<fs::path>base)
 {
     vector<fs::path>res;
     string str;
-    for (auto& p : fs::recursive_directory_iterator(pa[0]))
-    {
-        str = p.path().string().substr(pa[0].size());
-        for (int i = 1; i < pa.size(); i++)
-        {
-            if (!fs::exists(pa[i] + str)) {
-                res.push_back(p.path());
-                break;
-            }
-            else if (fs::last_write_time(pa[i] + str) > lastsync)
-            {
-                if (fs::last_write_time(p) < fs::last_write_time(pa[i] + str))
-                    fs::copy(pa[i] + str, p, fs::copy_options::overwrite_existing);
-                break;
-            }
-        }
-
-    }
-
     for (int i = 0; i < base.size(); i++)
     {
         if (!fs::exists(base[i]))
             res.push_back(base[i]);
+    }
+
+
+
+    for (int j = 0; j < base.size(); j++)
+    {
+        str = base[j].string().substr(pa[0].size());
+        for (int i = 1; i < pa.size(); i++)
+        {
+            if (!fs::exists(pa[i] + str)) {
+                res.push_back(base[j].string());
+                break;
+            }
+            else if (fs::last_write_time(pa[i] + str) > lastsync)
+            {
+                if (fs::exists(base[j])) {
+                    if (fs::last_write_time(base[j]) < fs::last_write_time(pa[i] + str))
+                        fs::copy(pa[i] + str, base[j], fs::copy_options::overwrite_existing);
+                    break;
+                }
+            }
+        }
+
     }
 
     for (int i = 1; i < pa.size(); i++)
@@ -85,74 +94,121 @@ vector<fs::path> we_are_conected_by_the_one_feeling(vector<string>pa, vector<fs:
 }
 
 
+void sync_cycle(vector<string> paths, vector<fs::path> &buf) {
+        buf = we_are_conected_by_the_one_feeling(paths, buf);
+        buf = there_can_be_only_one(paths, buf);
+}
 
-bool overlapping(bool menu) {
-    char c;    
+
+void update(int& counter, bool am, int cbuff, vector<string> paths, vector<fs::path> &buf) {
+    char c;
+    bool do_once = true;
+    if (counter >= 0 && am)
+    {
+        if (am) {
+            std::cout << "\r(A)Time remaining(type [esc or n] to open main menu): " << counter << std::flush;
+        }
+        Sleep(1000);
+        counter--;
+    }
+    if (am && counter == 0) 
+    {
+        sync_cycle(paths,buf);
+        Sleep(3000);
+        counter = cbuff;
+    }
+    else if (!am && counter == cbuff) {
+        sync_cycle(paths, buf);
+        std::cout << "Current dir was synced successfully" << std::endl;
+        std::cout << " (type[esc or n] to open main menu)" << std::endl;
+        counter--;
+    }
+}
+
+
+
+
+bool overlapping(bool menu, int& counter, bool& active, int cbuff, vector<string> paths, vector<fs::path> &buf) {
+    char c;
+    bool auto_mode = false;
+    
+
+    std::cout << "Welcome to the DIRSYNC service!" << std::endl;
+    std::cout << "DIRYNC service is a Windows service that enables easier user management." << std::endl;
+    std::cout << "This service can help u to sync ur local directory." << std::endl;
+    std::cout << "Do u wanna proceed and start syncin? [y/n or esc]" << std::endl;
+    std::cout << "(automode) " << auto_mode << std::flush;
 
     while (!menu) {
         if (_kbhit()) {
-            c = _getch(); 
+            c = _getch();
+            //cout << (int)c;
+            if ((int)c == 27 || (int)c == 110) {
+                menu = false;
+                system("cls");
+                active = false;
+                break;
+            }
+            else if ((int)c == 121) {
+                counter = cbuff;
+                menu = true;
+                system("cls");
+                break;
+            }
+            else if ((int)c == 97) {
+                auto_mode = !auto_mode;
+                std::cout << "\r(automode) " << auto_mode << std::flush;
+            }
+        }
+    }
+
+    while (menu) {
+        if (_kbhit()) {
+            c = _getch();
+            //std::cout << (int)c;
             if ((int)c == 27 || (int)c == 110) {
                 menu = false;
                 break;
             }
-            else if ((int)c == 121) {
-                menu = true;
-                break;
-            }
-
         }
+        update(counter, auto_mode, cbuff, paths,buf);
     }
-
+    system("cls");
     return menu;
+
 }
 
-
-void update(int t) {
-    while (t >= 0)
-    {
-        std::cout << "\rTime remaining: " << t << std::flush;
-        Sleep(1000);
-        t--;
-    }
-}
 
 int main() {
+
+    Sleep(1000);
+    int counter = 30;
+    int cbuff = counter;
+    bool menu = false;
+    bool active = true;
+    vector<string> paths;
     ofstream file;
     string str;
-    Sleep(1000);
-    bool menu = false;
-    
     if (fs::exists("config")) {
-        vector<string> paths;
+        
         ifstream file("config");
-        //bool sync = false;
-        cout << "Directory SYNC service" << endl;
-        cout << "If you want to begin syncing type [y/n]" << endl;
-        
-        while (menu = overlapping(menu)){
-            while (getline(file, str)) {
-                if (fs::exists(str)) {
-                    paths.push_back(str);
-                }
-                else
-                    cout << "no directory :" << "    " << str << endl;
+        while (getline(file, str)) {
+            if (fs::exists(str)) {
+                paths.push_back(str);
             }
-            //cout << "config read successfully" << endl;
-            vector<fs::path> buf;
-            buf.clear();
-            buf = there_can_be_only_one(paths, buf);
-        
-            update(30);
-
-            //Sleep(100);
-            //std::cout << "keep going" << endl;
-            buf = we_are_conected_by_the_one_feeling(paths, buf);
-            there_can_be_only_one(paths, buf);
-            //Timer_c(30);
-            //cout << "im here" << endl;
+            else
+                cout << "no directory :" << "    " << str << endl;
         }
+
     }
     else
         cout << "no config";
+
+    vector<fs::path> buf;
+    buf.clear();
+    buf = there_can_be_only_one(paths, buf);
+
+    while (active) {
+        menu = overlapping(menu, counter, active, cbuff, paths,buf);
+    }
 }
